028eb58cbc329aa467d977dbae121bd6abc98796
Julian Schramm <79840527+ace-of-aces@users.noreply.github.com>
2023-10-16 12:17:15 +0200

    added mermaid app config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Remaid from "./Remaid"
-import Header from "./Header"
+import Remaid from "./Remaid";
+import Header from "./Header";
 import Footer from "./Footer";
+import { MermaidConfig } from "mermaid";
 
 export default function App() {
+  const MERMAID_APP_CONFIG: Partial<MermaidConfig> = {
+    theme: "dark",
+    fontFamily: "system-ui",
+    themeVariables: {
+      quadrantPointFill: "#1971c2",
+      quadrant1Fill: "transparent",
+      quadrant2Fill: "transparent",
+      quadrant3Fill: "transparent",
+      quadrant4Fill: "transparent",
+    },
+  };
+
   return (
     <>
-    <Header />
+      <Header />
       <div className="flex flex-col gap-8 items-center w-full my-8 px-4 sm:px-0">
         <div className="w-full sm:w-1/2 max-w-lg">
           <Remaid
@@ -15,6 +28,7 @@ export default function App() {
             loading={
               <div className="aspect-square max-w-lg bg-gray-600 rounded-md animate-pulse" />
             }
+            config={MERMAID_APP_CONFIG}
           />
           <p className="italic text-sm">* : Non-programming language</p>
         </div>
@@ -26,6 +40,7 @@ export default function App() {
           loading={
             <div className="aspect-square max-w-lg bg-gray-600 rounded-md animate-pulse" />
           }
+          config={MERMAID_APP_CONFIG}
         />
         <hr className="w-full sm:w-1/2 max-w-lg my-8" />
         <Remaid
@@ -35,17 +50,7 @@ export default function App() {
           loading={
             <div className="aspect-square max-w-lg bg-gray-600 rounded-md animate-pulse" />
           }
-          config={{
-            theme: "dark",
-            fontFamily: "system-ui",
-            themeVariables: {
-              quadrantPointFill: "#1971c2",
-              quadrant1Fill: "transparent",
-              quadrant2Fill: "transparent",
-              quadrant3Fill: "transparent",
-              quadrant4Fill: "transparent",
-            },
-          }}
+          config={MERMAID_APP_CONFIG}
         />
       </div>
       <Footer />
